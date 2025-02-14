8d30d992e11ba5f372a9df6353b49d22067fa7d7
nemanja-jokic <57893634+nemanja-jokic@users.noreply.github.com>
2025-02-14 17:53:08 +0100

    PropTypes validation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,12 @@
-const About = ({ showIntro = true, imageSrc, title, description }) => {
+import PropTypes from 'prop-types';
+
+const About = ({ 
+  showIntro, 
+  imageSrc, 
+  title, 
+  description
+}) => {
+  
   return (
     <section className="flex flex-col items-center px-6 md:px-16 py-10 gap-12 max-w-6xl mx-auto mt-15">
       {/* Intro section */}
@@ -6,10 +14,10 @@ const About = ({ showIntro = true, imageSrc, title, description }) => {
         <div className="text-center pt-10">
           <h2 className="text-4xl font-semibold mb-6 font-[Cardo] text-gray-700">
             Discover the Art of Fine Dining at 
-            <span className="font-[Parisienne] text-amber-400"> Maison de l'Orchidée</span>
+            <span className="font-[Parisienne] text-amber-400"> {`Maison de l'Orchidée`}</span>
           </h2>
           <p className="text-lg text-gray-700 mb-8 font-[Cardo]">
-            Step into a world where fine dining meets timeless sophistication. At Maison de l'Orchidée, every dish is a masterpiece, crafted with passion and the finest ingredients. Let us take you on an unforgettable culinary journey, where every bite tells a story of tradition, creativity, and excellence.
+            {`Step into a world where fine dining meets timeless sophistication. At Maison de l'Orchidée, every dish is a masterpiece, crafted with passion and the finest ingredients. Let us take you on an unforgettable culinary journey, where every bite tells a story of tradition, creativity, and excellence.`}
           </p>
         </div>
       )}
@@ -34,5 +42,10 @@ const About = ({ showIntro = true, imageSrc, title, description }) => {
     </section>
   );
 };
-
+About.propTypes = {
+  description: PropTypes.string,
+  showIntro: PropTypes.string,
+  imageSrc: PropTypes.string,
+  title: PropTypes.string,
+}
 export default About;
